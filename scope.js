27ffb0063004a892da434628ac5900ff524892b2
pnlybubbles <pnlybubbles@gmail.com>
2017-04-06 20:09:23 +0900

    fix: eliminate es6 features in browser code

--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-const PREFIX_PREFIX = '_scope_'
+var PREFIX_PREFIX = '_scope_'
 
 module.exports = scope
 module.exports.bindPrefix = bindPrefix
@@ -6,14 +6,19 @@ module.exports.PREFIX_PREFIX = PREFIX_PREFIX
 
 function bindPrefix (node, prefix, scope) {
   if (node.nodeType === 1) {
-    const prefixed = Array.from(node.attributes).map((v) => v.name).find((v) => RegExp(`^${PREFIX_PREFIX}`).test(v))
+    var prefixed = Array.prototype.slice
+      .call(node.attributes)
+      .map(function (v) { return v.name })
+      .filter(function (v) { return RegExp('^' + PREFIX_PREFIX).test(v) })[0]
     if (scope === true ||      // if true, this node is scope root node
       (!scope && !prefixed) || // if true, this node has no prefix
       scope === prefixed) {    // if true, this node already has prefix same as scope root node
       node.setAttribute(prefix, '')
-      Array.from(node.childNodes).forEach((node) => {
-        bindPrefix(node, prefix, scope && prefixed)
-      })
+      Array.prototype.slice
+        .call(node.childNodes)
+        .forEach(function (node) {
+          bindPrefix(node, prefix, scope && prefixed)
+        })
     }
   }
 }
